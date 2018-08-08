c750dd0684386f665ca77064046992d0a12e1b28
Ryusei Shigaki <shigashan@me.com>
2018-08-08 12:35:24 +0900

    Initial commit

---
+++ app/src/main/java/com/hacku2018/slumpool/kiminitodoke/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hacku2018.slumpool.kiminitodoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
